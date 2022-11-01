0160f62c49e4595e1521a1d1a890dab1168d68c8
Vovandir <106840463+Vovandir@users.noreply.github.com>
2022-11-01 15:36:11 +0200

    cc

--- src/index.js
+++ src/index.js
@@ -1,3 +1,40 @@
 import './css/styles.css';
+import debounce from 'lodash.debounce';
+import Notiflix from 'notiflix';
+import { fetchCountries } from './js/fetchCountries';
 
+const debounce = require('lodash.debounce');
 const DEBOUNCE_DELAY = 300;
+const input = document.querySelector('#search-box');
+const ul = document.querySelector('.country-list');
+
+input.addEventListener('input',debounce(() => {motherFunction()
+}, DEBOUNCE_DELAY));
+  
+function motherFunction() {
+    fetchCountries(input.value.trim()).then((country) => {
+        if (country.length > 10) { Notiflix.Notify.info('Too many matches found. Please enter a more specific name.') }
+        else if (country.length > 1) { renderCountryList(country) }
+        else { renderOneCountry(country) }
+    })
+        .catch((error) => { console.log(error), Notiflix.Notify.failure('Oops, there is no country with that name'), ul.innerHTML = '' })    
+};
+
+function renderCountryList(country) {const markup = country
+    .map((country) => {
+        return `<li><p><img src='${country.flags.svg}'alt='flag' width ='25px'>  ${country.name.common}</p></li>`;
+    }).join("");
+  ul.innerHTML = markup;}
+
+function renderOneCountry(country) {
+     const markup = country
+    .map((country) => {
+      return `<li>
+          <p><img src='${country.flags.svg}'alt='flag' width ='25px'>  ${country.name.common}</p>
+          <p><b>Capital</b>: ${country.capital}</p>
+          <p><b>Population</b>: ${country.population}</p>          
+          <p><b>Language</b>: ${Object.values(country.languages)}</p>
+        </li>`;
+    }).join("");
+  ul.innerHTML = markup;
+}
